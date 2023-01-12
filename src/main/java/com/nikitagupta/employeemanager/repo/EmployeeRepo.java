4a550aebc9dd37946ef5b83a85097364cea5b987
nikitagupta06 <nikitagupta9696@gmail.com>
2023-01-12 23:34:36 +0530

    established employee and end points

---
+++ src/main/java/com/nikitagupta/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.nikitagupta.employeemanager.repo;
+
+import com.nikitagupta.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
---
+++ src/main/java/com/nikitagupta/employeemanager/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package com.nikitagupta.employeemanager.service;
+
+import com.nikitagupta.employeemanager.exception.UserNotFoundException;
+import com.nikitagupta.employeemanager.model.Employee;
+import com.nikitagupta.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+
+    }
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(
+                () -> new UserNotFoundException("User by id " + id + " not found")
+        );
+    }
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+    public void removeEmployeeById(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
